6899cb87cd7ae5f69cc96b77d587db74937dd8c8
Jonniek <kayhty@windowslive.com>
2024-04-09 20:46:16 +0300

    add option for current branch

--- lua/easypick/previewers.lua
+++ lua/easypick/previewers.lua
@@ -1,15 +1,16 @@
-local previewers = require "telescope.previewers"
-local putils = require "telescope.previewers.utils"
-local from_entry = require "telescope.from_entry"
+local previewers = require("telescope.previewers")
+local putils = require("telescope.previewers.utils")
+local from_entry = require("telescope.from_entry")
 local conf = require("telescope.config").values
 
-local default = function (opts)
+local default = function(opts)
 	opts = opts or {}
 	return previewers.vim_buffer_cat.new(opts)
 end
 
-local branch_diff = function (opts)
-	return previewers.new_buffer_previewer {
+local branch_diff = function(opts)
+	local current_branch = opts.current_branch or "HEAD"
+	return previewers.new_buffer_previewer({
 		title = "Git Branch Diff Preview",
 		get_buffer_by_name = function(_, entry)
 			return entry.value
@@ -30,17 +31,21 @@ local branch_diff = function (opts)
 					winid = self.state.winid,
 				})
 			else
-				putils.job_maker({ "git", "--no-pager", "diff", opts.base_branch .. "..HEAD", "--", file_name }, self.state.bufnr, {
-					value = file_name,
-					bufname = self.state.bufname,
-				})
+				putils.job_maker(
+					{ "git", "--no-pager", "diff", opts.base_branch .. ".." .. current_branch, "--", file_name },
+					self.state.bufnr,
+					{
+						value = file_name,
+						bufname = self.state.bufname,
+					}
+				)
 				putils.regex_highlighter(self.state.bufnr, "diff")
 			end
 		end,
-	}
+	})
 end
 
-local file_diff = function (opts)
+local file_diff = function(opts)
 	opts = opts or {}
 	return previewers.git_file_diff.new(opts)
 end
@@ -48,5 +53,5 @@ end
 return {
 	default = default,
 	branch_diff = branch_diff,
-	file_diff = file_diff
+	file_diff = file_diff,
 }
